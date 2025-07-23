3dc3a9477611b47ed5df1ba5e8c26108656d2299
Aragor85 <djameltelecom@gmail.com>
2025-07-24 01:04:05 +0200

    24072025 CI/CD

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,8 @@ FROM python:3.10-slim
 
 WORKDIR /app
 
-RUN apt-get update && apt-get install -y \
-    build-essential \
-    libglib2.0-0 \
-    libsm6 \
-    libxext6 \
-    libxrender-dev \
-    && rm -rf /var/lib/apt/lists/*
-
-COPY requirements.txt .
-
-RUN pip install --no-cache-dir -r requirements.txt
+COPY requirements.txt ./
+RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
@@ -20,4 +11,4 @@ RUN chmod +x start.sh
 
 EXPOSE 80
 
-CMD ["./start.sh"]
+CMD ["bash", "start.sh"]
--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Lancer FastAPI en arrière-plan
-gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 &
+# Lancer FastAPI avec gunicorn sur le port 8000 en arrière-plan
+gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 127.0.0.1:8000 &
 
-# Définir l'URL de l'API accessible pour Streamlit
+# Exporter l'URL de l'API pour Streamlit
 export API_URL="http://127.0.0.1:8000"
 
-# Lancer Streamlit en premier plan (sur port 80)
+# Lancer Streamlit en premier plan sur le port 80
 streamlit run streamlit_app/app.py --server.address 0.0.0.0 --server.port 80
