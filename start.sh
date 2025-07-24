624fadefbf130298176f784caabcbfef63dd08ba
Aragor85 <djameltelecom@gmail.com>
2025-07-24 19:13:29 +0200

    24072025 CI/CD

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 set -eux  # -e : exit on error, -u : error on unset vars, -x : log commands
 
 # 1. Démarrage de FastAPI avec Uvicorn sur 127.0.0.1:8000
-uvicorn app.main:app --host 127.0.0.1 --port 8000 &
+uvicorn api.main:app --host 127.0.0.1 --port 8000 &
 
 # 2. Exporter l'URL pour Streamlit
 export API_URL="http://127.0.0.1:8000"
