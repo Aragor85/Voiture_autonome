aa633711215ec8f56d363309df35265173d422ef
Aragor85 <djameltelecom@gmail.com>
2025-07-24 19:54:14 +0200

    24072025 CI/CD

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,15 @@
+FROM python:3.10-slim AS builder
+WORKDIR /app
+COPY requirements.txt .
+RUN pip wheel --no-cache-dir -r requirements.txt -w /wheels
+
 FROM python:3.10-slim
 WORKDIR /app
-
-COPY requirements.txt ./
-RUN pip install --upgrade pip \
- && pip install --no-cache-dir -r requirements.txt
+COPY --from=builder /wheels /wheels
+RUN pip install --no-cache-dir /wheels/*.whl
 
 COPY . .
-
-# Rendre le script exécutable
 RUN chmod +x start.sh
 
-# Exposer le port que Streamlit utilise
 EXPOSE 80
-
-# Lancement
-CMD ["bash", "start.sh"]
+ENTRYPOINT ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -eux
+set -eux  # -e: exit on error, -u: unset var → err, -x: echo cmds
 
-# 1. FastAPI en fond
-uvicorn api.main:app --host 127.0.0.1 --port 8000 &
+# 1. FastAPI sur toutes les interfaces, port 8000
+uvicorn api.main:app --host 0.0.0.0 --port 8000 &
 
+# 2. API_URL pour Streamlit
 export API_URL="http://127.0.0.1:8000"
 
-# 2. Streamlit en PID 1
-exec streamlit run app/streamlit_app.py --server.address 0.0.0.0 --server.port 80
+# 3. Streamlit en PID 1 sur le port 80
+exec streamlit run app/streamlit_app.py \
+     --server.address 0.0.0.0 \
+     --server.port 80
