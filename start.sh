bcd94108990d852cc9cd553de62513d54c0186a9
Aragor85 <djameltelecom@gmail.com>
2025-07-25 19:35:03 +0200

    25072025 19h30 CI/CD

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ echo "Démarrage de FastAPI (backend) sur le port 8000..."
 gunicorn api.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000 &
 
 # Exporter l'URL de l'API pour Streamlit
-export API_URL="http://127.0.0.1:8000"
+export API_URL="http://0.0.0.0:8000"
 
 # Lancer Streamlit en premier plan sur le port 80 (frontend)
 echo "Démarrage de Streamlit (frontend) sur le port 80..."
