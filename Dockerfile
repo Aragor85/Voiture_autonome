f0e058b1314e8bd10ba5609ff6906eee9a413fe6
Aragor85 <djameltelecom@gmail.com>
2025-07-27 02:11:44 +0200

    27072025 CI/CD

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,24 @@
+# Base légère avec Python 3.10
 FROM python:3.10-slim
 
+# Définir le répertoire de travail
 WORKDIR /app
 
+# Installer curl (pour le téléchargement du modèle)
 RUN apt-get update && apt-get install -y curl && rm -rf /var/lib/apt/lists/*
+
+# Installer les dépendances Python
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Copier tout le projet
 COPY . .
 
+# Rendre le script de démarrage exécutable
 RUN chmod +x start.sh
+
+# Exposer le port Streamlit (port public)
 EXPOSE 8080
 
+# Lancer le script au démarrage
 CMD ["./start.sh"]
--- start.sh
+++ start.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set -e
 
-MODEL_PATH=app/model/unet_vgg16_best.h5
+MODEL_PATH=/app/api/model/unet_vgg16_best.h5
 MODEL_URL="https://modelevgg16unetstorage.blob.core.windows.net/modelevgg16unetstorage/unet_vgg16_best.h5"
 
+# Créer le dossier si nécessaire
 mkdir -p $(dirname "$MODEL_PATH")
 
 if [ ! -f "$MODEL_PATH" ]; then
-  echo "📥 Téléchargement du modèle..."
+  echo " Téléchargement du modèle..."
   curl -L -o "$MODEL_PATH" "$MODEL_URL"
-  echo "✅ Modèle téléchargé."
+  echo " Modèle téléchargé."
 else
-  echo "✅ Modèle déjà présent."
+  echo " Modèle déjà présent."
 fi
 
 # Lancer FastAPI en arrière-plan
-echo "🚀 Lancement de l'API FastAPI..."
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+echo " Lancement de FastAPI..."
+uvicorn api.main:app --host 0.0.0.0 --port 8000 &
 
 # Lancer Streamlit
 PORT=${PORT:-8080}
-echo "🚀 Lancement de Streamlit sur le port $PORT..."
-streamlit run app/streamlit_app.py --server.port=$PORT --server.address=0.0.0.0
+echo " Lancement de Streamlit sur le port $PORT..."
+streamlit run app/streamlit_app.py --server.port $PORT --server.address 0.0.0.0
